d638d0f9fd5ab7e2f3b9572c8a1a34242044d4c2
shubhamc <shubh.chodankar@gmail.com>
2016-08-28 16:31:32 +0530

    Updates the template to convert into angular-webapp structure

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
-var app = angular.module("DeviceTracker", []);
-app.controller("devicetrackController", function($scope) {
-    $scope.testString = 'Controller working as expected';
-   
+var routerApp = angular.module('deviceTracker', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('devices', {
+            // we'll get to this in a bit 
+            url: '/listdevices',
+            templateUrl: 'home.html'      
+        })
+        //fetch stationary devices state
+        .state('StationaryDevices', {
+            // we'll get to this in a bit 
+            url: '/fetchstationarydevices',
+            templateUrl: 'home.html'      
+        })
+        //fetch overspeeding devices state
+        .state('OverSpeedingDevices', {
+            // we'll get to this in a bit 
+            url: '/fetchoverspeedingdevices',
+            templateUrl: 'home.html'      
+        })
+        //Geo dwell
+        .state('Geodwell', {
+            // we'll get to this in a bit 
+            url: '/geodwell',
+            templateUrl: 'home.html'      
+        })
+        //SystemHealth
+        .state('SystemHealth', {
+            // we'll get to this in a bit 
+            url: '/systemhealth',
+            templateUrl: 'home.html'      
+        })
+        //DevicePositions
+        .state('DevicePositions', {
+            // we'll get to this in a bit 
+            url: '/devicepositions',
+            templateUrl: 'home.html'      
+        });
+
+        
 });
--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,4 +1,4 @@
 //Create controllers to be used in the application
-app.controller("devicetrackController", function($scope) {
+routerApp.controller("devicetrackController", function($scope) {
     $scope.testString = 'Welcome to angular.js application ..inprogress basic structure';
 });

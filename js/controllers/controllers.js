e99466eecb7b1834058bd92a1da6bc474b69908a
shubhamc <shubh.chodankar@gmail.com>
2016-08-28 17:48:28 +0530

    Updates client to send requests to listdevices api

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,4 +1,18 @@
 //Create controllers to be used in the application
-app.controller("devicetrackController", function($scope) {
+app.controller("devicetrackController", function($scope, $http) {
     $scope.testString = 'Welcome to angular.js application ..inprogress basic structure';
+    $scope.deviceList = [];
+	//Send a request to server to get list of devices connected
+	$scope.getDevicelist = function(){
+    console.log('Trying to connect to node.js server');
+		$http.get('http://localhost:8080/tracker/listDevicesOnSystem')
+        .success(function(data) {
+            $scope.deviceList = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+	};
+
 });
